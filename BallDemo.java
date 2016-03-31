e22a41b8c867e8f526119774e05e4c5ed2345003
liveworldstrike <lazodellamas12@gmail.com>
2016-03-31 11:00:12 +0200

    añadida nueva clase boxBall y metodo boxBounce

--- BallDemo.java
+++ BallDemo.java
@@ -68,4 +68,52 @@ public class BallDemo
             }
         }
     }
+    
+    /**
+     * dibujar un rectángulo sin relleno en la pantalla y dentro tantas bolas como se indiquen 
+     * 
+     */
+    public void boxBounce(int numeroBolas)
+    {
+        int ground = 400;   // position of the ground line
+
+        myCanvas.setVisible(true);
+
+        // draw the ground
+        myCanvas.drawLine(50, ground, 550, ground);
+        //array que guarda las bolas
+        ArrayList<BoxBall> arrayBolas = new ArrayList<>();
+
+        // crate and show the balls
+        for(int i = 0; i < numeroBolas; i++)
+        {
+            Random aleatorio = new Random();
+            int radio =  aleatorio.nextInt(100);
+
+            int red =  aleatorio.nextInt(256);
+            int green =  aleatorio.nextInt(256);
+            int blue =  aleatorio.nextInt(256);
+
+            int x =  aleatorio.nextInt(220);
+            int y =  aleatorio.nextInt(210);
+
+
+            arrayBolas.add(new BoxBall(x, y, radio, new Color(red,green,blue), ground, myCanvas));
+            arrayBolas.get(i).draw();
+        }
+
+        // make them bounce
+        boolean finished =  false;
+        while(!finished) {
+            myCanvas.wait(50);           // small delay
+            for(BoxBall elemento : arrayBolas)
+            {
+                elemento.move();
+                // stop once ball has travelled a certain distance on x axis
+                if(elemento.getXPosition() >= 550) {
+                    finished = true;
+                }
+            }
+        }
+    }
 }
---
+++ BoxBall.java
@@ -0,0 +1,102 @@
+import java.awt.*;
+import java.awt.geom.*;
+
+
+/**
+ * Class BallDemo - a short demonstration showing animation with the 
+ * Canvas class. 
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 2011.07.31
+ */
+
+public class BoxBall 
+{
+    private Canvas canvas;
+    private int ySpeed = 1;
+    private int ballDegradation = 2;
+    private Ellipse2D.Double circle;
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;
+    private final int groundPosition;      // y position of ground
+
+    /**
+     * Constructor for objects of class BouncingBall
+     *
+     * @param xPos  the horizontal coordinate of the ball
+     * @param yPos  the vertical coordinate of the ball
+     * @param ballDiameter  the diameter (in pixels) of the ball
+     * @param ballColor  the color of the ball
+     * @param groundPos  the position of the ground (where the wall will bounce)
+     * @param drawingCanvas  the canvas to draw this ball on
+     */
+    public BoxBall (int xPos, int yPos, int ballDiameter, Color ballColor,
+                         int groundPos, Canvas drawingCanvas)
+    {
+        xPosition = xPos;
+        yPosition = yPos;
+        color = ballColor;
+        diameter = ballDiameter;
+        groundPosition = groundPos;
+        canvas = drawingCanvas;
+    }
+
+    /**
+     * Draw this ball at its current position onto the canvas.
+     **/
+    public void draw()
+    {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }    
+
+    /**
+     * Move this ball according to its position and speed and redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+        erase();
+
+        // compute new position
+        yPosition += ySpeed;
+        xPosition +=2;
+
+        // check if it has hit the ground
+        if(yPosition >= (groundPosition - diameter) && ySpeed > 0) {
+            yPosition = (int)(groundPosition - diameter);
+            ySpeed = -ySpeed + ballDegradation; 
+        }
+
+        // draw again at new position
+        draw();
+    }    
+
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
+    }
+}
+
+    
